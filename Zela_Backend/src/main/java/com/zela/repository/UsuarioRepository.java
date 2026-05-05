a1c58a4b6c7d0729d83c627add3dd00e4a28c007
guilhermehctrl07 <guilhermeh.a.s2307@gmail.com>
2026-05-05 20:16:13 -0300

    Alteração Back

--- Zela_Backend/src/main/java/com/zela/repository/UsuarioRepository.java
+++ Zela_Backend/src/main/java/com/zela/repository/UsuarioRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
-    // Aqui o Spring já cria para você métodos como:
-    // .save(), .findAll(), .findById(), .deleteById()
 }
